cb3a38537879f572aba992018899fea8e1c6b9fa
leobewater <mmbee888@gmail.com>
2023-10-21 15:24:58 -0400

    load fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,29 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+// make splash screen viewable when loading
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />;
+  // setting fonts
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../asssets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../asssets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../asssets/fonts/DMSans-Regular.ttf"),
+  });
+
+  // loading fonts
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
 };
 
 export default Layout;
